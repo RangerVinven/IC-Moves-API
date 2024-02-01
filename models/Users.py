9d04edc0468f03529cf738be30a296335ea7283d
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-01 21:55:36 +0000

    Created the User_Email_Password_Fields_Optional model

--- models/Users.py
+++ models/Users.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class User_Email_Password(BaseModel):
     email: str
     password: str
+
+class User_Email_Password_Fields_Optional(BaseModel):
+    email: str | None = None
+    password: str | None = None
