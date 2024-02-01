c93801d25011ca83c8e04718d48d5423eb4ce6c8
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-01 22:22:40 +0000

    Added check to make sure the property isn't already saved

--- routes/saved_properties.py
+++ routes/saved_properties.py
@@ -17,7 +17,7 @@ async def get_saved_properties(request: Request):
     return cursor.fetchall()
 
 @router.get("/{property_id}")
-async def save_properties(property_id: str, request: Request, response: Response):
+async def save_property(property_id: str, request: Request, response: Response):
     session_token = get_session_token_from_request(request)
 
     # Redirects if the user isn't logged in
@@ -25,21 +25,27 @@ async def save_properties(property_id: str, request: Request, response: Response
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return
 
+    # Ensures the property exists
     cursor.execute("SELECT id FROM Properties WHERE id=%s;", (property_id,))
     properties = cursor.fetchall()
 
-    # Redirects if the property doesn't exist
     if len(properties) == 0:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return
 
-    cursor.execute("SELECT id FROM Users WHERE session_token=%s", (session_token,))
+    # Gets the user's id
+    cursor.execute("SELECT id FROM Users WHERE session_token=%s;", (session_token,))
     user_id = cursor.fetchone()["id"]
+
+    # Doesn't add to the database if it's already saved
+    cursor.execute("SELECT property_id FROM SavedProperties WHERE user_id=%s AND property_id=%s;", (user_id, property_id))
+    if len(cursor.fetchall()) != 0:
+        return
 
     cursor.execute("INSERT INTO SavedProperties (user_id, property_id) VALUES (%s, %s);", (user_id, property_id))
     db.commit()
 
-    return RedirectResponse(url="/")
+    return
 
 @router.delete("/{property_id}")
 async def unsave_property(property_id: str, request: Request, response: Response):
