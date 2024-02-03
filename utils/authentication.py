8ad8eb97ae1930667d1baba3494f436468ce9a7b
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-03 23:19:49 +0000

    Created check_if_email_is_unique

--- utils/authentication.py
+++ utils/authentication.py
@@ -4,6 +4,8 @@ import hashlib
 from fastapi import Request
 from models.Users import User_Email_Password
 
+from utils.database_connector import cursor
+
 def sha256(string_to_hash):
     return hashlib.sha256(string_to_hash.encode()).hexdigest()
 
@@ -11,9 +13,19 @@ def generate_session_token(user: User_Email_Password):
     return sha256(str(time()) + user.email + user.password)
 
 def get_session_token_from_request(request: Request):
-    session_token = request.cookies.get("session_token")
+    session_token = request.headers.get("session_token")
 
     if session_token is None:
         return ""
     else:
         return session_token
+    
+# Checks if the email doesn't exist in the database
+def check_if_email_is_unique(email):
+    cursor.execute("SELECT id FROM Users WHERE email=%s;", (email,))
+    users = cursor.fetchall()
+
+    if users == []:
+        return True
+    else:
+        return False
